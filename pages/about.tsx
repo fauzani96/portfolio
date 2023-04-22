5669eec7adb69531657f55c744eaad7e06a6bfa2
fauzani <muhammadfauzanisa@gmail.com>
2023-04-22 22:21:16 +0700

    unfixed slicing landing

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,7 +5,7 @@ import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Link from '../src/Link'
-import ProTip from '../src/ProTip'
+// import ProTip from '../src/ProTip'
 import Copyright from '../src/Copyright'
 
 const About: NextPage = () => {
@@ -25,7 +25,7 @@ const About: NextPage = () => {
             Go to the home page
           </Button>
         </Box>
-        <ProTip />
+        {/* <ProTip /> */}
         <Copyright />
       </Box>
     </Container>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,39 +2,56 @@ import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import type {NextPage} from 'next'
 import Copyright from '../src/Copyright'
+import {Avatar, Button, Chip, Typography} from '@mui/material'
 
 const Home: NextPage = () => {
   return (
-    <Container maxWidth="lg">
+    <Container>
       <Box
         sx={{
-          my: 4,
           display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
           alignItems: 'center',
+          flexDirection: {
+            xs: 'column', // theme.breakpoints.up('xs')
+            lg: 'row',
+          },
+          gap: 10,
+          minHeight: 'calc(100vh - 84px)',
         }}
       >
-        this is homebro this is homebro this is homebro this is homebro this is
-        homebro this is homebro this is homebro this is homebro this is homebro
-        this is homebro this is homebro this is homebro this is homebro this is
-        homebro this is homebro this is homebro this is homebro this is homebro
-        this is homebro this is homebro this is homebro this is homebro this is
-        homebro this is homebro this is homebro this is homebro this is homebro
-        this is homebro this is homebro this is homebro this is homebro this is
-        homebro this is homebro this is homebro this is homebro this is homebro
-        this is homebro this is homebro this is homebro this is homebro this is
-        homebro this is homebro this is homebro this is homebro this is homebro
-        this is homebro this is homebro this is homebro this is homebro this is
-        homebro this is homebro this is homebro this is homebro this is homebro
-        this is homebro this is homebro this is homebro this is homebro this is
-        homebro this is homebro this is homebro this is homebro this is homebro
-        this is homebro this is homebro this is homebro this is homebro this is
-        homebro this is homebro this is homebro this is homebro this is homebro
-        this is homebro this is homebro this is homebro this is homebro this is
-        homebro this is homebro this is homebro
-        <Copyright />
+        <Avatar
+          sx={{width: {xs: 200, lg: 400}, height: {xs: 200, lg: 400}}}
+          src="https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png"
+        />
+        <Box
+          sx={{
+            display: 'flex',
+            gap: 2,
+            flexDirection: 'column',
+          }}
+        >
+          <Typography variant="h5">Front End Developer</Typography>
+          <Typography variant="h3" fontWeight={500}>
+            Muhammad Fauzani Shah Allam
+          </Typography>
+          <Typography variant="h6">
+            There wasn&apos;t a bird in the sky, but that was not what caught
+            her attention. It was the clouds. The deep green that isn&apos;t the
+            color of clouds, but came with these. She knew what was coming and
+            she hoped she was prepared.
+          </Typography>
+          <Box sx={{display: 'flex', gap: 2}}>
+            <Chip
+              label="Download CV"
+              clickable
+              variant="outlined"
+              color="primary"
+            />
+            <Chip label="Contact" clickable variant="filled" color="primary" />
+          </Box>
+        </Box>
       </Box>
+      <Copyright />
     </Container>
   )
 }
